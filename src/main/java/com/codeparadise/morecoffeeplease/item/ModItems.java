6f1b208803b051f50681403bb2dff45e2c737c3d
ForestParadiseOrigene <andres.jcv.678@gmail.com>
2023-10-01 05:41:57 -0500

    Some fixes.
    Now the Normal Coffee and Hot Coffee got the movement speed.

--- src/main/java/com/codeparadise/morecoffeeplease/item/ModItems.java
+++ src/main/java/com/codeparadise/morecoffeeplease/item/ModItems.java
@@ -16,11 +16,18 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, MoreCoffeePlease.MOD_ID);
 
-    // Hot coffee
+    // Normal coffee
     public static final RegistryObject<Item> COFFEE = ITEMS.register("coffee",
             () -> new Item(new Item.Properties()
                     .tab(ModItemGroup.COFFEE_GROUP)
                     .stacksTo(1)
+                    .food(new Food.Builder()
+                            .nutrition(4)
+                            .saturationMod(2)
+                            .alwaysEat()
+                            .effect(new EffectInstance(Effects.MOVEMENT_SPEED,600,0),1.0F)
+                            .build()
+                    )
             )
     );
 
@@ -28,13 +35,14 @@ public class ModItems {
     public static final RegistryObject<Item> HOT_COFFEE = ITEMS.register("hot_coffee",
             () -> new Item(new Item.Properties()
                     .tab(ModItemGroup.COFFEE_GROUP)
+                    .stacksTo(1)
                     .food(new Food.Builder()
                             .nutrition(4)
                             .saturationMod(2)
                             .alwaysEat()
-                            .effect(new EffectInstance(Effects.FIRE_RESISTANCE, 200, 0), 1.0F)
+                            .effect(new EffectInstance(Effects.FIRE_RESISTANCE, 600, 0), 1.0F)
+                            .effect(new EffectInstance(Effects.MOVEMENT_SPEED,600,0),1.0F)
                             .build()
-
                     )
             )
     );
